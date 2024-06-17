fd49670f2691a33210f9428884798f4cf6611990
Júlio César Pereira <jc.imp10@gmail.com>
2024-06-17 23:15:42 +0100

    reload

--- BlogClubeLeituraSolution/BlogClubeLeitura/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace BlogClubeLeitura.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public string? ProfilePicture { get; set; }
-    }
-}
